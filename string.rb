feadae5727da982a05c70aa0c502356f9eb1945f
parkyes90 <parkyes90@gmail.com>
2021-01-10 16:05:42 +0900

    string methods

---
+++ string.rb
@@ -0,0 +1,15 @@
+sentence=  "My name is Parkyes90"
+
+first_name = "park"
+last_name = "yes"
+
+p sentence
+
+puts first_name + " " + last_name
+
+puts "#{first_name} #{last_name}"
+
+puts first_name.upcase
+puts first_name.class
+puts first_name.methods
+puts "Hi \#{first_name}"
