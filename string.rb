6164bbe45f99d108d9dac215f8a78eacb9150479
parkyes90 <parkyes90@gmail.com>
2021-01-10 16:08:36 +0900

    use io

--- string.rb
+++ string.rb
@@ -13,3 +13,11 @@ puts first_name.upcase
 puts first_name.class
 puts first_name.methods
 puts "Hi \#{first_name}"
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts first_name
+
+puts "Enter a number to multiply by 2"
+input = gets.chomp.to_i
+puts input * 2
