f5682a8939b7bc4e8abb95c3c1a81ba6128f38f1
parkyes90 <parkyes90@gmail.com>
2021-01-11 20:34:48 +0900

    test number methods2

--- number.rb
+++ number.rb
@@ -1,5 +1,28 @@
-puts 1+2
+puts 1 + 2
 nil
 puts 10 / 4.0
-20.times { print "-"}
-puts "hello".to_i
+20.times { print '-' }
+puts 'hello'.to_i
+
+def print_operations
+  operations = [
+    1 + 2,
+    1 * 2,
+    1 / 2,
+    1 - 2,
+    1 % 2,
+    20.to_f,
+    22.odd?,
+    22.even?,
+    1 == 2,
+    3 == 3,
+    5 && 6,
+    5 || 6,
+    rand(10),
+    5.to_s
+  ]
+
+  operations.each { |x| puts x }
+end
+
+print_operations if __FILE__ == $0
