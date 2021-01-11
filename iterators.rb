72974666e7dd9b3070f17bb85c1a476571887a03
parkyes90 <parkyes90@gmail.com>
2021-01-11 21:00:04 +0900

    test array & iterators methods

---
+++ iterators.rb
@@ -0,0 +1,14 @@
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+puts a.length
+a << 10
+print a
+puts a.last
+puts a.first
+a.unshift('Test')
+a.append('Test')
+print(a)
+print a.uniq
+puts a.empty?
+b = []
+puts b.empty?
+puts a.include?('Test')
