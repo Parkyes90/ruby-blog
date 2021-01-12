78d1fda97c24b0c0e8a5abb334afffd6d69f35f2
parkyes90 <parkyes90@gmail.com>
2021-01-12 20:46:07 +0900

    add bcrypt lib and compare pass

--- classes/student.rb
+++ classes/student.rb
@@ -1,11 +1,13 @@
-class Student
-  attr_accessor :first_name, :last_name, :email, :username
+require 'bcrypt'
 
-  def initialize(first_name, last_name, email, username)
+class Student
+  attr_accessor :first_name, :last_name, :email, :username, :password
+  def initialize(first_name, last_name, email, username, password)
     @first_name = first_name
     @last_name = last_name
     @username = username
     @email = email
+    @password = BCrypt::Password.create(password)
   end
 
   def to_s
@@ -13,10 +15,11 @@ class Student
   end
 end
 
-student1 = Student.new('test1', 'last', 'email', 'username')
+student1 = Student.new('test1', 'last', 'email', 'username', 'password1')
 puts student1
 
 puts student1.first_name
 puts student1.last_name
 puts student1.email
 puts student1.username
+puts student1.password == 'password1'
