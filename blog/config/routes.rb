df3fd2550e74d2ce81939352e1152a3fa2e972ed
parkyes90 <parkyes90@gmail.com>
2021-01-22 22:18:58 +0900

    add login controller & view

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -8,5 +8,8 @@ Rails
     post 'users', to: 'users#create'
     resources :articles
     resources :users, except: [:new]
+    get "login", to: "sessions#new"
+    post "login", to: "sessions#create"
+    delete "logout", to: "sessions#destroy"
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
