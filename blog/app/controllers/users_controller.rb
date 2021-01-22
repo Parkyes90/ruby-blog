5b61c6d0363c1be68c5a0d2e5e0da1b833f7d547
parkyes90 <parkyes90@gmail.com>
2021-01-22 22:41:54 +0900

    Refactor usersController

--- blog/app/controllers/users_controller.rb
+++ blog/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -12,12 +13,9 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'Your account information was successfully updated'
       redirect_to @user
@@ -44,5 +42,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:user])
   end
 end
