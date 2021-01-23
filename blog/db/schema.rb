acd1427ac95970a7bb55783e0046f3a7fedf43a3
parkyes90 <parkyes90@gmail.com>
2021-01-23 16:36:06 +0900

    Add Admin field to users

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_111708) do
+ActiveRecord::Schema.define(version: 2021_01_23_073451) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_01_20_111708) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
