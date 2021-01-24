e48a4bafd60ebb8574d92b2864c5b961b2a7b1e6
parkyes90 <parkyes90@gmail.com>
2021-01-24 16:26:50 +0900

    Add categories controller test

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -8,8 +8,9 @@ Rails
     post 'users', to: 'users#create'
     resources :articles
     resources :users, except: [:new]
-    get "login", to: "sessions#new"
-    post "login", to: "sessions#create"
-    delete "logout", to: "sessions#destroy"
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    resources :categories, except: [:delete]
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
