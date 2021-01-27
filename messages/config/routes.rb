917ea289186ba4f81ac69635430c500173a99d45
parkyes90 <parkyes90@gmail.com>
2021-01-27 22:53:34 +0900

    Add channels

--- messages/config/routes.rb
+++ messages/config/routes.rb
@@ -7,5 +7,7 @@ Rails
     post 'login', to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy'
     post 'message', to: 'messages#create'
+
+    mount ActionCable.server, at: '/cable'
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
