090d7c440ecd7868409b0853dcb42e6f116eded7
parkyes90 <parkyes90@gmail.com>
2021-02-02 21:54:54 +0900

    Add Track stocks

--- finance-tracker/app/controllers/user_stocks_controller.rb
+++ finance-tracker/app/controllers/user_stocks_controller.rb
@@ -1,3 +1,17 @@
 class UserStocksController < ApplicationController
-
+  def create
+    stock = Stock.check_db(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker])
+      if stock
+        stock.save
+      else
+        flash[:alert] = "Please enter a valid symbol to search"
+        return redirect_to my_portfolio_path
+      end
+    end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+    redirect_to my_portfolio_path
+  end
 end
--- finance-tracker/config/routes.rb
+++ finance-tracker/config/routes.rb
@@ -6,6 +6,6 @@ Rails
     devise_for :users
     get 'my_portfolio', to: 'users#my_portfolio'
     get 'search_stock', to: 'stocks#search'
-    resources :user_stocks
+    resources :user_stocks, only: [:create]
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
