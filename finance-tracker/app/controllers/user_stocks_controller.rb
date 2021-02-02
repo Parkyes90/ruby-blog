b06917fa5acb05ef0cb284002395bfcf8a8fef58
parkyes90 <parkyes90@gmail.com>
2021-02-02 22:30:26 +0900

    Add functions to remove tracking

--- finance-tracker/app/controllers/user_stocks_controller.rb
+++ finance-tracker/app/controllers/user_stocks_controller.rb
@@ -14,4 +14,13 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user: current_user, stock:stock).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from your portfolio"
+    redirect_to my_portfolio_path
+  end
+
 end
