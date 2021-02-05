7837c84cfa4a5981b12216b63541a8221c6d14bf
parkyes90 <parkyes90@gmail.com>
2021-02-05 22:52:17 +0900

    Apply Send Email

--- photo-app/config/environment.rb
+++ photo-app/config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.google.com",
+  :port => "587",
+  :authentication => :plain,
+  :user_name => ENV["GOOGLE_USERNAME"],
+  :password => ENV["GOOGLE_PASSWORD"],
+  :domain => "google.com",
+  :enable_starttls_auto => true,
+}
