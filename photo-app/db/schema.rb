412da81bc41812dd009447b5c4051a3f28b599e9
parkyes90 <parkyes90@gmail.com>
2021-02-06 23:44:16 +0900

    Add Payment model

--- photo-app/db/schema.rb
+++ photo-app/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_130919) do
+ActiveRecord::Schema.define(version: 2021_02_06_143803) do
+  create_table "payments", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
